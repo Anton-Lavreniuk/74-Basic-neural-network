7179bc74fabf29928befd54c64eb4a2e6d7fc422
Anton-Lavreniuk <lvrnk.a.o@gmail.com>
2022-12-13 23:14:59 +0200

    An implementation of a Perceptron calculating bitwise OR. Generates random weights, then runs N iterations to adjust them.

--- main.cpp
+++ main.cpp
@@ -1,9 +1,10 @@
 #include <iostream>
 
 
-void Perceptron(int input1,int input2, int bias,int lr,int output,int *weights){
+void Perceptron(int input1,int input2, float bias,float lr,int output,float *weights){
 
-    int outputP=input1*weights[0]+input2*weights[2]+bias&weights[3];
+
+    int outputP=input1*weights[0]+input2*weights[1]+bias*weights[2];
 
     if(outputP>0){
         outputP=1;
@@ -14,21 +15,25 @@ void Perceptron(int input1,int input2, int bias,int lr,int output,int *weights){
     }
 
     int error = output-outputP;
-    weights[0]=error*input1*lr;
-    weights[1]=error*input2*lr;
-    weights[2]=error*bias*lr;
-    std::cout<<output<<":"<<outputP<<" W1: "<<weights[0]<<" W2: "<<weights[1]<<" WB: "<<weights[2]<<" Error:"<<error<<std::endl;
+    weights[0]+=error*input1*lr;
+    weights[1]+=error*input2*lr;
+    weights[2]+=error*bias*lr;
+    std::cout<<input1<<"|"<<input2<<" "<<"Expected:"<<output<<" Calculated:"<<outputP<<" W1: "<<weights[0]<<" W2: "<<weights[1]<<" WB: "<<weights[2]<<" Error:"<<error<<std::endl;
 }
-
+float GenWeight(){
+    return static_cast <float> (rand()) / static_cast <float> (RAND_MAX);
+}
 int main() {
-    int lr=1;
-    auto bias=1;
-    int weights[3]={rand(),rand(),rand()};
-    for(int i = 0;i<50;i++){
-        Perceptron(0,1,bias,lr,1,weights);
+    float lr=0.1;
+    float bias=1;
+    int n = 50;
+    float weights[3]={GenWeight(),GenWeight(),GenWeight()};
+    for(int i = 0;i<n;i++){
         Perceptron(1,1,bias,lr,1,weights);
         Perceptron(1,0,bias,lr,1,weights);
+        Perceptron(0,1,bias,lr,1,weights);
         Perceptron(0,0,bias,lr,0,weights);
+
 
     }
 
