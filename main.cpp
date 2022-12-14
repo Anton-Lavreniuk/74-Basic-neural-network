075c7ff11e7e8232236f0f32a8fce3d2b673e47b
Anton-Lavreniuk <lvrnk.a.o@gmail.com>
2022-12-14 23:57:26 +0200

    An implementation of a Perceptron calculating bitwise OR. Generates random weights, then runs N iterations to adjust them.Improved output.

--- main.cpp
+++ main.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-
+#include <iomanip>
 
 void Perceptron(int input1,int input2, float bias,float lr,int output,float *weights){
 
@@ -18,7 +18,7 @@ void Perceptron(int input1,int input2, float bias,float lr,int output,float *wei
     weights[0]+=error*input1*lr;
     weights[1]+=error*input2*lr;
     weights[2]+=error*bias*lr;
-    std::cout<<input1<<"|"<<input2<<" "<<"Expected:"<<output<<" Calculated:"<<outputP<<" W1: "<<weights[0]<<" W2: "<<weights[1]<<" WB: "<<weights[2]<<" Error:"<<error<<std::endl;
+    std::cout<<std::fixed<<std::setprecision(4)<<"    "<<input1<<"  |   "<<input2<<"   | "<<"Expected:"<<output<<"     | Calculated:"<<outputP<<"     |W1: "<<weights[0]<<"|W2: "<<weights[1]<<"|WB: "<<weights[2]<<"      | Error:"<<error<<"           |"<<((error==0) ? "SUCCESS":"ERROR")<<std::endl;
 }
 float GenWeight(){
     return static_cast <float> (rand()) / static_cast <float> (RAND_MAX);
@@ -26,8 +26,10 @@ float GenWeight(){
 int main() {
     float lr=0.1;
     float bias=1;
-    int n = 50;
+    int n = 1;
     float weights[3]={GenWeight(),GenWeight(),GenWeight()};
+    std::cout<<" Bit 1 | Bit 2 | Expected value | Calculated value | Weight 1 | Weight 2 | Weight of bias | Calculation error | Result "<<std::endl;
+
     for(int i = 0;i<n;i++){
         Perceptron(1,1,bias,lr,1,weights);
         Perceptron(1,0,bias,lr,1,weights);
